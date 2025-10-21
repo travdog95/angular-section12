4aa2508b914c8941c91ffe6a59615aeac5426fca
Travis Peters <travis@tkopeters.com>
2025-10-21 10:45:59 -0600

    initial commit

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,37 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        // observe: 'response',
+        // observe: 'events',
+      })
+      .subscribe({
+        next: (resData) => {
+          this.places.set(resData.places);
+          console.log(resData);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      console.log('AvailablePlacesComponent destroyed');
+      subscription.unsubscribe();
+    });
+  }
+}
